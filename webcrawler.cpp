a03847e471f5a6e132c7ae875463909f13974113
abhiga <agaurav@purdue.edu>
2015-07-02 16:49:43 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -1,4 +1,13 @@
 
 #include "webcrawler.h"
+#include "SimpleHTMLParser.h"
 
 // Add your implementation here
+WebCrawler::WebCrawler(int maxUrls, int nInitialURls,  const char ** initialURLs)
+{
+	this -> _maxUrls = maxUrls;
+		
+  // Allocate space for _urlArray
+  // insert the initialURls
+  // Update _maxUrls, _headURL and _tailURL
+}
