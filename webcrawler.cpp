cd15f2a3e750f3ea790acaceb40fd3adbb31c2cc
abhiga <agaurav@purdue.edu>
2015-07-05 16:16:21 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -15,7 +15,7 @@ WebCrawler::WebCrawler(int maxUrls, int nInitialURls,  const char ** initialURLs
 	printf("%d\n", nInitialURls);
 	for (int i = 0; i < nInitialURls; i++) {
 		_urlArray[i]._url = strdup(initialURLs[i]);
-		//_urlArray[i]._description = "empty";
+		_urlArray[i]._description = "empty";
 	}
 	// Update _maxUrls, _headURL and _tailURL
 	_maxUrls = maxUrls;
