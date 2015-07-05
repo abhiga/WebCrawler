79fa6857ca5f4f623c576368d310bea567486cdd
abhiga <agaurav@purdue.edu>
2015-07-05 09:40:37 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -52,7 +52,7 @@ void WebCrawler::onAnchorFound(char * url){
 	//check if the URL is absolute
 	else if(('a' <= url[0] && url[0] <= 'z')||('A' <= url[0] && url[0] <= 'Z')) {
 		//bool flag1 = true;
-		//char *temp = strdup(_urlArray[_tailURL-1]._url);
+		char *temp = strdup(_urlArray[_tailURL-1]._url);
 		/*for(int i = strlen(temp) - 1; i > 8; i--) {
 			if(temp[i] =='/') {
 				temp[i+1] = '\0';
@@ -60,10 +60,10 @@ void WebCrawler::onAnchorFound(char * url){
 				flag = false;	
 				break;
 			}
-		}*/
-		if (flag) {
-			//strcat(strcat(temp,"/"),url);
 		}
+		if (flag) {
+			strcat(strcat(temp,"/"),url);
+		}*/
 	}
 }
 void WebCrawler::crawl()
