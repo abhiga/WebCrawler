1242298afb13dfd7083715b96f0b01cf4af321bd
abhiga <agaurav@purdue.edu>
2015-07-05 13:44:20 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -109,7 +109,7 @@ void WebCrawler::crawl()
 		
 		//Get the first 500 characters (at most) of the document without tags. Add this 
 		//  description to theURL record for this URL.
-		_urlArray[_headURL]._description = "empty";
+		//_urlArray[_headURL]._description = "empty";
 		_headURL++;
 		//Find all the hyperlinks of this document and add them to the
 		//_urlArray and _urlToUrlRecord if they are not already in the
@@ -123,7 +123,7 @@ void WebCrawler::crawl()
 	}
 	for(int i = 0; i < _tailURL;i++) {
 			printf("%s\n", _urlArray[i]._url);
-			printf("%s\n", _urlArray[i]._description);
+			//printf("%s\n", _urlArray[i]._description);
 		}
 }
 
