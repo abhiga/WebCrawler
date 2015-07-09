cedeb0466c3f42d74e49b9e18f69e78ecce91b8c
abhiga <agaurav@purdue.edu>
2015-07-09 19:00:22 -0400

    Commit web-crawler

--- SimpleHTMLParser.cpp
+++ SimpleHTMLParser.cpp
@@ -44,9 +44,9 @@ SimpleHTMLParser::parse(char * buffer, int n)
 			else if (match(&b,"<FRAME ")) {
 				state = FRAME;
 			}
-			//else if (match(&b,"<TITLE>")) {
-				//state = TITLE;
-			//}
+			else if (match(&b,"<TITLE>")) {
+				state = TITLE;
+			}
 			else if (match(&b,"<META ")) {
 				state = META;
 			}
